367d2600c5e6edac495bbbe5bd35f2390052e64d
neelamsamtani <neelamsamtani@outlook.com>
2017-03-20 00:55:44 +0000

    step four complete

--- angular.js
+++ angular.js
@@ -3,9 +3,16 @@ var app = angular.module("myshoppinglist", []);
 app.controller("myctrl", function($scope) {
     $scope.products = ["Milk", "Bread", "Cheese"];
     $scope.additem = function(){
-        $scope.products.push($scope.addme);
+        $scope.errortext = "";
+        if (!$scope.addme) {return;}
+        if ($scope.products.indexOf($scope.addme) == -1) {
+            $scope.products.push($scope.addme);
+        } else {
+            $scope.errortext = "The item is already in your shopping list.";
+        }
     };
     $scope.removeitem = function(x){
+        $scope.errortext = "";
         $scope.products.splice(x, 1);
     };
 });
