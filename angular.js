7f689c2d9d56bbe91fd1ceb13c4956df0dbbb870
neelamsamtani <neelamsamtani@outlook.com>
2017-03-20 00:37:31 +0000

    step two complete

--- angular.js
+++ angular.js
@@ -1,4 +1,8 @@
 var app = angular.module("myshoppinglist", []); 
+
 app.controller("myctrl", function($scope) {
     $scope.products = ["Milk", "Bread", "Cheese"];
+    $scope.additem = function(){
+        $scope.products.push($scope.addme);
+    };
 });
